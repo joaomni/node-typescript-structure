037df18915376f71aa4ff667703c911a1dd34365
joaoqsl <joaojpmoreira25@gmail.com>
2020-06-22 12:33:54 -0300

    Add ESLint & Jest

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import "@controllers/UserController";
+import express from 'express'
+import '@controllers/UserController'
 
 const app = express()
 
 const APP_PORT = 3333
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello Wolrd' })
+  return response.json({ message: 'Hello Wolrd' })
 })
 
 app.listen(APP_PORT, () => {
-    console.log(`[*] Server running in http://localhost:${APP_PORT}`)
+  console.log(`[*] Server running in http://localhost:${APP_PORT}`)
 })
